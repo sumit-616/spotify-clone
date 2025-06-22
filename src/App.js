36128ea1e64f0bc9f37ea6c01684c03de23929ee
sumit-616 <sumit616794@gmail.com>
2025-06-22 12:41:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ class App extends Component {
   static audio;
 
   componentDidMount() {
-    const isOnCallbackPage = window.location.pathname === '/callback';
     let hashParams = {};
     let e,
       r = /([^&;=]+)=?([^&;]*)/g,
@@ -32,14 +31,15 @@ class App extends Component {
       hashParams[e[1]] = decodeURIComponent(e[2]);
     }
 
-    if (!hashParams.access_token && !isOnCallbackPage) {
+    const redirectUri = window.location.origin + '/callback';
+
+    if (!hashParams.access_token) {
       window.location.href =
-        'https://accounts.spotify.com/authorize?client_id=2009f4d7b76c42e0ae2749419dfb952c&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=' +
-        window.location.origin +
-        '/callback';
-    } else if (hashParams.access_token) {
+        'https://accounts.spotify.com/authorize?client_id=230be2f46909426b8b80cac36446b52a&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri='+ redirectUri;
+    } else {
       this.props.setToken(hashParams.access_token);
     }
+
   }
 
   componentWillReceiveProps(nextProps) {
